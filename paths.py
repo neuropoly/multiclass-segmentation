a16bb56df7ed7ecdffc90f9d4dfffabe114fb049
frpau_local <frpau_local@spalding.neuro.polymtl.ca>
2018-09-18 16:37:57 -0400

    remove remaining sct dependencies

--- paths.py
+++ paths.py
@@ -2,50 +2,10 @@
 
 # paths to the txt files containing the paths to the nifti files (input and gt)
 
-#training_data = "/home/frpau_local/data/filenames_training.txt"
-training_data = "/home/frpau_local/data/filenames_training_cord_all.txt"
+training_data = "/home/frpau_local/data/filenames_training.txt"
 validation_data = "/home/frpau_local/data/filenames_validation.txt"
-
-#training_data = "/home/frpau_local/data/filenames_training_corrected.txt"
-#validation_data = "/home/frpau_local/data/filenames_validation_corrected.txt"
-
-#training_data = "/home/frpau_local/data/filenames_training_luisa_gretel.txt"
-#validation_data = "/home/frpau_local/data/filenames_validation_luisa_gretel.txt"
-
-#training_data = "/home/frpau_local/data/filenames_training_nawm.txt"
-#training_data = "/home/frpau_local/data/filenames_training_nawm_all.txt"
-#validation_data = "/home/frpau_local/data/filenames_validation_nawm.txt"
-
-#training_data = "/home/frpau_local/data/filenames_lesion_training.txt"
-#validation_data = "/home/frpau_local/data/filenames_lesion_validation.txt"
-
-#training_data = "/home/frpau_local/data/filenames_lesion_training_corrected.txt"
-#validation_data = "/home/frpau_local/data/filenames_lesion_validation_corrected.txt"
-
-#training_data = "/home/frpau_local/data/filenames_lesion_training_corrected_w_eva.txt"
-#validation_data = "/home/frpau_local/data/filenames_lesion_validation_corrected_w_eva.txt"
-
-#training_data = "/home/frpau_local/data/filenames_training_subpial.txt"
-#validation_data = "/home/frpau_local/data/filenames_validation_subpial.txt"
-
-#training_data = "/home/frpau_local/data/filenames_training_subpial_bis.txt"
-#validation_data = "/home/frpau_local/data/filenames_validation_subpial_bis.txt"
-
-#training_data = "/home/frpau_local/data/filenames_training_subpial_ter.txt"
-#validation_data = "/home/frpau_local/data/filenames_validation_subpial_ter.txt"
-
-#training_data = "/home/frpau_local/data/filenames_training_subpial_4.txt"
-#validation_data = "/home/frpau_local/data/filenames_validation_subpial_4.txt"
-
-#training_data = "/home/frpau_local/data/filenames_training_subpial_5.txt"
-#training_data = "/home/frpau_local/data/filenames_training_subpial_6.txt"
-#validation_data = "/home/frpau_local/data/filenames_validation_subpial_ter.txt"
 
 # path to the json file containing the hyper-parameters
 parameters = "/home/frpau_local/data/parameters.json" 
-#parameters = "/home/frpau_local/data/parameters_NoPoolASPP.json"
 
-# paths to sct directories (used to change input resolution or orientation in pre_processing)
-sct_scripts = "/home/frpau_local/spinalcordtoolbox/scripts"
-sct_dir = "/home/frpau_local/spinalcordtoolbox/python/lib/python2.7/site-packages/spinalcordtoolbox"
 
